366fb04fb1ee349ef115834ca461a7f9408dc10c
sweetwong <yuanlai5chaos@gmail.com>
2019-11-14 21:19:29 +0800

    添加IntelliJ IDEA的settings

---
+++ src/LeetCode/Item8.java
@@ -0,0 +1,15 @@
+package LeetCode;
+
+class Item8 {
+
+
+  private static boolean isNumber(char c) {
+    return c >= 48 && c <= 57;
+  }
+
+  private static boolean isSigned(char c) {
+    return c == '-' | c == '+';
+  }
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
+import edu.princeton.cs.algs4.Bag;
+
 class Main {
 
+  public static void main(String[] args) {
+  }
 }
