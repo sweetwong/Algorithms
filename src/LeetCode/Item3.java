c33c354f92299a26a809b64ef34edccd5f773e2d
SweetWong <yuanlai5chaos@gmail.com>
2019-11-18 21:44:11 +0800

    重新完成了一次力扣第三题，要充分理解滑动窗口的思想（快慢指针）

--- src/LeetCode/Item3.java
+++ src/LeetCode/Item3.java
@@ -3,25 +3,26 @@ package LeetCode;
 import java.util.HashSet;
 import java.util.Set;
 
-// https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/
+/**
+ * 无重复字符的最长子串
+ * https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/
+ */
 class Item3 {
 
   public static int lengthOfLongestSubstring(String s) {
     Set<Character> set = new HashSet<>();
-    int times = 0;
     int res = 0, l = 0, r = 0;
     while (r < s.length()) {
-      times++;
-      System.out.println("times: " + times + " ,l: " + l + ", r: " + r + ", res: " + res +
-        ", char[l]: " + s.charAt(l) + ", char[r]: " + s.charAt(r));
       char curr = s.charAt(r);
+      // 如果Set不包含，快指针进一步
       if (!set.contains(curr)) {
         set.add(curr);
         r++;
         res = Math.max(res, r - l);
-      } else {
+      }
+      // 如果Set包含，满指针进一步
+      else {
         set.remove(s.charAt(l));
-        System.out.print("curr: " + curr + "\ns.charAt(l): " + s.charAt(l) + "\n\n");
         l++;
       }
     }
