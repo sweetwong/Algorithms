e6b3992aa34d84c32442cca00047a7ba90c132be
SweetWong <yuanlai5chaos@gmail.com>
2020-01-06 07:49:06 +0800

    good

--- src/Solution.java
+++ src/Solution.java
@@ -1,35 +1,20 @@
-import sort.nlogn.HeapSort;
-import sort.nlogn.MergeSort;
-import sort.nlogn.QuickSort;
-import util.ArrayUtils;
-import util.Time;
-
-import java.util.Arrays;
-
 class Solution {
+    public boolean isMatch(String text, String pattern) {
+      boolean[][] dp = new boolean[text.length() + 1][pattern.length() + 1];
+      dp[text.length()][pattern.length()] = true;
 
-  public static void main(String[] args) {
-
-    for (int i = 0; i < 5; i++) {
-
-      int[] nums1 = ArrayUtils.createRandomArrays(10000000);
-      System.out.println("归并排序");
-      Time.watch(() -> MergeSort.sort(nums1));
-
-      int[] nums2 = ArrayUtils.createRandomArrays(10000000);
-      System.out.println("快速排序");
-      Time.watch(() -> QuickSort.sort(nums2));
-
-      int[] nums3 = ArrayUtils.createRandomArrays(10000000);
-      System.out.println("堆排序");
-      Time.watch(() -> HeapSort.sort(nums3));
-
-      int[] nums4 = ArrayUtils.createRandomArrays(10000000);
-      System.out.println("系统排序");
-      Time.watch(() -> Arrays.sort(nums4));
-
-      System.out.println();
+      for (int i = text.length(); i >= 0; i--){
+        for (int j = pattern.length() - 1; j >= 0; j--){
+          boolean first_match = (i < text.length() &&
+            (pattern.charAt(j) == text.charAt(i) ||
+              pattern.charAt(j) == '.'));
+          if (j + 1 < pattern.length() && pattern.charAt(j+1) == '*'){
+            dp[i][j] = dp[i][j+2] || first_match && dp[i+1][j];
+          } else {
+            dp[i][j] = first_match && dp[i+1][j+1];
+          }
+        }
+      }
+      return dp[0][0];
     }
-
-  }
 }
---
+++ src/leet_code/Item133.java
@@ -0,0 +1,10 @@
+package leet_code;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+
+class Item133 {
+
+}
