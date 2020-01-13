db4241e1fcb9ed567b811f1d0bb0d235a774c8dc
王思威 <yuanlai5chaos@gmail.com>
2020-01-13 10:11:45 +0800

    新添加了第十题, 正则表达式匹配的备注和理解

--- src/Solution.java
+++ src/Solution.java
@@ -1,11 +1,7 @@
-import design_pattern.singleton.Enum;
-
-import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.concurrent.atomic.AtomicInteger;
-
 class Solution {
 
   public static void main(String[] args) {
+
   }
 
 }
--- src/leet_code/Item10.java
+++ src/leet_code/Item10.java
@@ -1,43 +1,97 @@
 package leet_code;
 
+/**
+ * Given an input string (s) and a pattern (p), implement regular expression matching with support for '.' and '*'.
+ *
+ * '.' Matches any single character.
+ * '*' Matches zero or more of the preceding element.
+ * The matching should cover the entire input string (not partial).
+ *
+ * Note:
+ *
+ * s could be empty and contains only lowercase letters a-z.
+ * p could be empty and contains only lowercase letters a-z, and characters like . or *.
+ *
+ * Example 1:
+ * Input:
+ * s = "aa"
+ * p = "a"
+ * Output: false
+ * Explanation: "a" does not match the entire string "aa".
+ *
+ * Example 2:
+ * Input:
+ * s = "aa"
+ * p = "a*"
+ * Output: true
+ * Explanation: '*' means zero or more of the preceding element, 'a'. Therefore, by repeating 'a' once, it becomes "aa".
+ *
+ * Example 3:
+ * Input:
+ * s = "ab"
+ * p = ".*"
+ * Output: true
+ * Explanation: ".*" means "zero or more (*) of any character (.)".
+ *
+ * Example 4:
+ * Input:
+ * s = "aab"
+ * p = "c*a*b"
+ * Output: true
+ * Explanation: c can be repeated 0 times, a can be repeated 1 time. Therefore, it matches "aab".
+ *
+ * Example 5:
+ * Input:
+ * s = "mississippi"
+ * p = "mis*is*p*."
+ * Output: false
+ */
 class Item10 {
 
   /**
+   * 递归法, 优点: 好理解
+   *        缺点: 重复计算子问题, 类似于斐波那契函数的递归计算, 效率很低
    *
-   * s = "aab";
-   * p = "c*a*b";
-   * <p>
-   * s = "mississippi"
-   * p = "mis*is*p*."
-   * <p>
-   * 如果 p >= 2 且 p[1] == '*'
-   * 可以把问题分解成两个子问题
-   * <p>
-   * 否则就看第一个是否匹配,并将问题缩小一个规模
-   *
+   *        如何改善: 使用动态规划 dp
    */
-  public static boolean isMatch(String s, String p) {
-    // 递归的终止条件
-    if (p.isEmpty()) return s.isEmpty();
+  public boolean isMatch(String s, String p) {
+
+    int len = p.length();
+
+    // 当p的长度为0, 递归的终止条件
+    if (len == 0) return s.isEmpty();
 
     // 判断第一个字符是否匹配
     boolean firstMatch = (!s.isEmpty() && (p.charAt(0) == s.charAt(0) || p.charAt(0) == '.'));
 
-    // 如果pattern的长度大于等于2且第二个为星
-    if (p.length() >= 2 && p.charAt(1) == '*') {
-      return (isMatch(s, p.substring(2)) || (firstMatch && isMatch(s.substring(1), p)));
+    // 当p的长度为1, 这种情况不存在'*'问题
+    if (len == 1) {
+      return s.length() == 1 && firstMatch;
     }
-    // 如果pattern的长度为1或者大于等于2但是第二不为星
+
+    // p的长度为0和1都被拦截了, 下面的都是p长度大于1的情况
+
+    // 当p的长度大于1, 且第二字符为'*'
+    if (p.charAt(1) == '*') {
+      // 当第一个字符匹配, 大问题可以分为两种子问题, s进1 或者 p进2
+      if (firstMatch) {
+        return isMatch(s.substring(1), p) || isMatch(s, p.substring(2));
+      }
+      // 当第一个字符不匹配, 减小问题范围, 只能 p进2(忽略掉'*'和其前一个字符)
+      else {
+        return isMatch(s, p.substring(2));
+      }
+    }
+    // 当p的长度大于1, 且第二个字符不为'*', 问题规模缩小1
     else {
       return firstMatch && isMatch(s.substring(1), p.substring(1));
     }
 
   }
 
-  public static void main(String[] args) {
-    String s = "";
-    String p = "a*";
-    System.out.println(isMatch(s, p));
+  public boolean isMatchDp(String s, String p) {
+    return false;
   }
+
 
 }
