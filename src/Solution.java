662bae27575a78067237659b00a1da5dbe2d25da
Sweet <1040565286@qq.com>
2020-03-14 10:40:39 +0800

    今天是3月14日，最近身体有点不舒服，而且还要准备面试。总之，不管怎么样，积极向上的心态和勇往无前的决心，总是可以支撑着你做很多事情的。

--- src/Solution.java
+++ src/Solution.java
@@ -1,15 +1,7 @@
 class Solution {
 
-  public int maxProfit(int[] prices) {
-    if (prices.length == 0) return 0;
-
-    int maxProfit = 0;
-    int minPrice = prices[0];
-    for (int price : prices) {
-      maxProfit = Math.max(maxProfit, price - minPrice);
-      minPrice = Math.min(minPrice, price);
-    }
-    return maxProfit;
+  public static void main(String[] args) {
+    System.out.println("好好学习");
   }
 
 }
