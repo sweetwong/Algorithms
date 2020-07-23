bc972b5ca49551acb5ed57380e7fd5ca9adcd3f4
wangsw <wangsw@seirobotics.net>
2020-07-23 21:10:16 +0800

    新完成最小路径和

---
+++ src/leet_code/Item64_最小路径和_自顶向下.java
@@ -0,0 +1,82 @@
+package leet_code;
+
+import java.util.Arrays;
+
+/**
+ * 给定一个包含非负整数的 m x n 网格，请找出一条从左上角到右下角的路径，使得路径上的数字总和为最小。
+ *
+ * 说明：每次只能向下或者向右移动一步。
+ *
+ * 示例:
+ *
+ * 输入:
+ * [
+ *   [1,3,1],
+ *   [1,5,1],
+ *   [4,2,1]
+ * ]
+ * 输出: 7
+ * 解释: 因为路径 1→3→1→1→1 的总和最小。
+ */
+public class Item64_最小路径和_自顶向下 {
+
+    public static int minPathSum(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] dp = new int[m + 1][n + 1];
+        for (int[] d : dp) {
+            Arrays.fill(d, -1);
+        }
+        return minPathSum(grid, dp, grid.length, grid[0].length, 0, 0);
+    }
+
+    private static int minPathSum(int[][] grid, int[][] dp, int m, int n, int i, int j) {
+        if (i >= m || j >= n) {
+            return Integer.MAX_VALUE;
+        }
+
+        if (i == m - 1 && j == n - 1) {
+            return grid[m - 1][n - 1];
+        }
+
+        int down = dp[i + 1][j];
+        int right = dp[i][j + 1];
+
+        if (down == -1) {
+            down = minPathSum(grid, dp, m, n, i + 1, j);
+            dp[i + 1][j] = down;
+        }
+        if (right == -1) {
+            right = minPathSum(grid, dp, m, n, i, j + 1);
+            dp[i][j + 1] = right;
+        }
+
+        return grid[i][j] + Math.min(down, right);
+    }
+
+    public static void main(String[] args) {
+        int[][] grid = {
+                {3, 8, 6, 0, 5, 9, 9, 6, 3, 4, 0, 5, 7, 3, 9, 3},
+                {0, 9, 2, 5, 5, 4, 9, 1, 4, 6, 9, 5, 6, 7, 3, 2},
+                {8, 2, 2, 3, 3, 3, 1, 6, 9, 1, 1, 6, 6, 2, 1, 9},
+                {1, 3, 6, 9, 9, 5, 0, 3, 4, 9, 1, 0, 9, 6, 2, 7},
+                {8, 6, 2, 2, 1, 3, 0, 0, 7, 2, 7, 5, 4, 8, 4, 8},
+                {4, 1, 9, 5, 8, 9, 9, 2, 0, 2, 5, 1, 8, 7, 0, 9},
+                {6, 2, 1, 7, 8, 1, 8, 5, 5, 7, 0, 2, 5, 7, 2, 1},
+                {8, 1, 7, 6, 2, 8, 1, 2, 2, 6, 4, 0, 5, 4, 1, 3},
+                {9, 2, 1, 7, 6, 1, 4, 3, 8, 6, 5, 5, 3, 9, 7, 3},
+                {0, 6, 0, 2, 4, 3, 7, 6, 1, 3, 8, 6, 9, 0, 0, 8},
+                {4, 3, 7, 2, 4, 3, 6, 4, 0, 3, 9, 5, 3, 6, 9, 3},
+                {2, 1, 8, 8, 4, 5, 6, 5, 8, 7, 3, 7, 7, 5, 8, 3},
+                {0, 7, 6, 6, 1, 2, 0, 3, 5, 0, 8, 0, 8, 7, 4, 3},
+                {0, 4, 3, 4, 9, 0, 1, 9, 7, 7, 8, 6, 4, 6, 9, 5},
+                {6, 5, 1, 9, 9, 2, 2, 7, 4, 2, 7, 2, 2, 3, 7, 2},
+                {7, 1, 9, 6, 1, 2, 7, 0, 9, 6, 6, 4, 4, 5, 1, 0},
+                {3, 4, 9, 2, 8, 3, 1, 2, 6, 9, 7, 0, 2, 4, 2, 0},
+                {5, 1, 8, 8, 4, 6, 8, 5, 2, 4, 1, 6, 2, 2, 9, 7}
+        };
+        int res = minPathSum(grid);
+        System.out.println(res);
+    }
+
+}
