56afe9af42f8dd2bd3f1797948752950d69700cc
sweetwong <yuanlai5chaos@gmail.com>
2020-08-24 23:57:41 +0800

    sync

---
+++ src/javas/io/file/Foo.java
@@ -0,0 +1,11 @@
+package javas.io.file;
+
+import javas.io.IOUtils;
+
+public class Foo {
+
+    public static void main(String[] args) {
+        IOUtils.deleteFile("E:\\F\\Project\\other\\Algorithms\\src\\javas\\io\\file\\Foo.java");
+    }
+
+}
--- src/javas/utils/Utils.java
+++ src/javas/utils/Utils.java
@@ -19,18 +19,16 @@ public class Utils {
         return builder.toString();
     }
 
-<<<<<<< HEAD
     public static void printStackStrace() {
         StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
         for (int i = 2; i < stackTrace.length; i++) {
             System.out.println(stackTrace[i]);
         }
     }
-=======
+
     public static String formatString(long data) {
         DecimalFormat df = new DecimalFormat("#,###");
         return df.format(data);
     }
 
->>>>>>> 4446c4a3cbb770947557a86f1564a7eb2d1dd74c
 }
